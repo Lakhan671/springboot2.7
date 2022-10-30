0902ea00e858c43459c106dd008bf62f7409d08d
Lakhan671 <lakhans671@gmail.com>
2022-10-30 22:02:58 +0530

    boot 2.7

---
+++ src/main/java/com/lakhan/springboot27/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.lakhan.springboot27.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HomeController {
+    @GetMapping("/get")
+    public String get(){
+        return "get mapping call.";
+
+    }
+}
